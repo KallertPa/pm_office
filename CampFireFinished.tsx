4654bb5c2f6f49143c4dc322e6288595a8115c7b
KallertPa <kallertp@gmail.com>
2021-04-17 10:55:13 +0200

    Resolved tileset issues
    Removed tsx files

--- CampFireFinished.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="CampFireFinished" tilewidth="32" tileheight="32" tilecount="20" columns="10">
- <image source="../CampFireFinished.png" width="320" height="64"/>
-</tileset>
--- city_inside.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="city_inside" tilewidth="32" tileheight="32" tilecount="384" columns="24">
- <image source="../LPC_city_inside/city_inside.png" width="768" height="512"/>
-</tileset>

f5ef5f7ef184b459a8d483e573dd54ec05916a81
KallertPa <kallertp@gmail.com>
2021-04-18 17:18:39 +0200

    Eddited scripts

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
-let helloWorldPopup;
 
 // Open the popup when we enter a given zone
 helloWorldPopup = WA.onEnterZone('pumpkin', () => {
-    WA.openPopup("pumpkin_popup", 'Bring me Giuliano!', [
-        {
-            label: "Close",
-            className: "primary",
-            callback: (popup) => {
-                // Close the popup when the "Close" button is pressed.
-                popup.close();
-            }
-        }
-    ]);
+    WA.sendChatMessage("Bring me Giuliano!", 'Pumpkin Man');
 });
 
 // Close the popup when we leave the zone.
 WA.onLeaveZone('pumpkin', () => {
-    helloWorldPopup.close();
+    WA.sendChatMessage("Do not return without him!", 'Pumpkin Man');
 });
